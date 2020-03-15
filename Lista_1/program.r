9b6ffec621a5e526db9c83557d762055d2f08ab3
KevinHomeAlone <kevinpieprz@gmail.com>
2020-03-16 00:17:17 +0100

    sd dziala

--- Lista_1/program.r
+++ Lista_1/program.r
@@ -1,3 +1,4 @@
 print("Krysiaczek programista")
 liczby = c(4.41, 3.92, 5.38, 2.77, 4.60, 5.14, 3.93, 3.15, 4.67, 2.98, 0.001, 10.45)
 print(paste("Średnia: ",mean(liczby)))
+print(paste("Odchylenie: ", sd(liczby)))
